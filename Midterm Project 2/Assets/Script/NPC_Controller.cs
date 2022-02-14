16f4ea8143905e083e959e2be8f0c51d33ee50b5
mlquadri <mslhquadri@gmail.com>
2022-02-14 08:32:04 -0600

    Changed levels

--- Midterm Project 2/Assets/Script/NPC_Controller.cs
+++ Midterm Project 2/Assets/Script/NPC_Controller.cs
@@ -6,6 +6,7 @@ using System.IO;
 public class NPC_Controller : MonoBehaviour
 {
     public DialogController dialogController;
+    public StoryController storyController;
     public string scriptFileName = "Assets/Character Dialogue/TutorialDialogue.txt";
     public string script;
     // Start is called before the first frame update
@@ -22,6 +23,22 @@ public class NPC_Controller : MonoBehaviour
         catch 
         {
             Debug.Log(this.gameObject.name+" Dialogue file not loaded");
+        }
+        if (this.gameObject.transform.name == "NPC1") 
+        {
+            if (!storyController.person1IsAlive) { this.gameObject.SetActive(false); }
+        }
+        else if (this.gameObject.transform.name == "NPC2")
+        {
+            if (!storyController.person2IsAlive) { this.gameObject.SetActive(false); }
+        }
+        else if(this.gameObject.transform.name == "NPC3")
+        {
+            if (!storyController.person3IsAlive) { this.gameObject.SetActive(false); }
+        }
+        else if(this.gameObject.transform.name == "NPC4")
+        {
+            if (!storyController.person4IsAlive) { this.gameObject.SetActive(false); }
         }
     }
 
